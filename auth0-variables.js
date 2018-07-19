e05ca85e3f2b344528347ba066fb51dfe54d8169
Amit Arora <mailamitarora@gmail.com>
2018-07-18 20:59:37 -0400

    auth variable

---
+++ auth0-variables.js
@@ -0,0 +1,4 @@
+var AUTH0_CLIENT_ID='VybTnhRvwYkLFuC2BhQ9peFEHkLhyvGA';
+var AUTH0_DOMAIN='amitpoc.auth0.com';
+var AUTH0_CALLBACK_URL='http://localhost:3000/callback';
+var AUTH0_AUDIENCE='http://localhost:3001/api';
